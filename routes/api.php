15f2f4ab292500aac2eb37a6cc7761ecc5b831d2
Douglas Bezerra da Silva <douglas.silva@pgmais.com.br>
2020-06-29 22:19:02 -0300

    adjusts

--- routes/api.php
+++ routes/api.php
@@ -12,9 +12,9 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-header("Access-Control-Allow-Origin: *");
-//header("Access-Control-Allow-Methods: GET,HEAD,POST,PUT,PATCH,OPTIONS,DELETE");
-//header("Access-Control-Allow-Headers: Content-Type, X-Requested-With");
+//header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET,HEAD,POST,PUT,PATCH,OPTIONS,DELETE");
+header("Access-Control-Allow-Headers: Content-Type, X-Requested-With");
 
 
 Route::apiResource('products','ProductsController');
